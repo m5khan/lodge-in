371c34eddd3937ba3e6357f654b407a1136ca40e
Shoaib Khan <mshoaibkhan91@gmail.com>
2020-03-17 12:09:08 +0100

    configure react app with webpack

---
+++ app/components/app.tsx
@@ -0,0 +1,17 @@
+import React, { Component} from "react";
+
+interface MyProps {
+    [key:string]:any;
+}
+
+interface MyState {
+    [key:string]:any
+}
+
+const App = () => {
+    return(
+        <h1>hello world this is react running</h1>
+    )
+}
+
+export default App;
--- webpack.config.ts
+++ webpack.config.ts
@@ -30,7 +30,57 @@ const serverConfig = {
 }
 
 
-module.exports = [
-    serverConfig
+const webConfig = {
+    mode: "production",
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "inline-source-map",
+    entry: ["./app/index.tsx"],
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".jsx"]
+    },
+    target: 'web', // <=== can be omitted as default is 'web'
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'client.js'
+    },
+    
+    module: {
+        rules: [
+            {
+                test: /\.ts(x?)$/,
+                //exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"]
+              }
+        ]
+    }
+    
+    // When importing a module whose path matches one of the following, just
+    // assume a corresponding global variable exists and use that instead.
+    // This is important because it allows us to avoid bundling all of our
+    // dependencies, which allows browsers to cache those libraries between builds.
+    // externals: {
+    //     "react": "React",
+    //     "react-dom": "ReactDOM"
+    // }
+}
 
+
+module.exports = [
+    serverConfig,
+    webConfig
 ] 
