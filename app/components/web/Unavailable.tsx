d6e9d9a346233b9787440bf6a4d320abc6284f16
Shoaib Khan <mshoaibkhan91@gmail.com>
2020-04-17 03:30:19 +0200

    add divider

--- app/components/web/Unavailable.tsx
+++ app/components/web/Unavailable.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Box, Typography } from '@material-ui/core';
+import { Box, Divider, Typography } from '@material-ui/core';
 import MoodBadIcon from '@material-ui/icons/MoodBad';
 
 const Container = styled.div`
@@ -14,12 +14,15 @@ type cssProp = {
 const Unavailable: React.FC = () => {
     return (
         <Box display="flex" alignItems="center" justifyContent="center" css={{ height: '100%' }}>
+            
             <Container pad={30}>
                 <MoodBadIcon style={{fontSize: '50px', width: '100%'}} color="action"></MoodBadIcon>
                 <Typography align="center" variant="body1" gutterBottom paragraph color="textSecondary" component="p">
                     There are no bookings available for this property to show.
                 </Typography>
+                <Divider />
             </Container>
+            
         </Box>
         );
 }
